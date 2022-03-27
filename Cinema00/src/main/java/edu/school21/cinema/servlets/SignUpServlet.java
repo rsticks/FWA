1fe9041c00cc89f74bd2c9905f695c3702dd601e
rsticks <al.paha@yandex.ru>
2022-03-27 23:42:12 +0300

    signup fix

--- Cinema00/src/main/java/edu/school21/cinema/servlets/SignUpServlet.java
+++ Cinema00/src/main/java/edu/school21/cinema/servlets/SignUpServlet.java
@@ -3,6 +3,7 @@ package edu.school21.cinema.servlets;
 import edu.school21.cinema.User.service.UserService;
 import edu.school21.cinema.config.Config;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DuplicateKeyException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -16,7 +17,7 @@ import java.io.IOException;
 
 @WebServlet(value = "/signup", name = "SignUp", description = "Sing Up")
 public class SignUpServlet extends HttpServlet {
-
+    private static final String SIGNUP_URL = "/WEB-INF/html/signup.html";
     private UserService userService;
 
     @Override
@@ -28,17 +29,21 @@ public class SignUpServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        req.getRequestDispatcher("/WEB-INF/html/signuup.html").forward(req, resp);
+        req.getRequestDispatcher(SIGNUP_URL).forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         resp.setContentType("text/html");
         String phoneNum = req.getParameter("phoneNum");
         String password = req.getParameter("password");
         String firstName = req.getParameter("firstname");
         String lastName = req.getParameter("lastname");
-        userService.saveUser(phoneNum, password, firstName, lastName);
-
+        try {
+            userService.saveUser(phoneNum, password, firstName, lastName);
+        } catch (DuplicateKeyException e) {
+            req.getRequestDispatcher(SIGNUP_URL).forward(req, resp);
+        }
+        resp.sendRedirect("/signin");
     }
 }
